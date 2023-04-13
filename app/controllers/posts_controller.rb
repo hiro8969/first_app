53426de2e130d193ad64525c2e3f6cebd3cfb433
hiro8969 <129718390+hiro8969@users.noreply.github.com>
2023-04-13 12:28:41 +0900

    手直し

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,12 @@
 class PostsController < ApplicationController
   def index
-  @posts = Post.all
+    @posts = Post.all
   end
- 
+
   def new
   end
 
   def create
-    Post.create(content: params[:content]) 
+    Post.create(content: params[:content])
   end
 end
